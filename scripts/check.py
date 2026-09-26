817532a2c838911b61adfe68c428033ce571f8af
mkpoli <git@mkpo.li>
2026-09-26 13:00:19 +0900

    chore: version 0.5.0

--- scripts/check.py
+++ scripts/check.py
@@ -43,7 +43,7 @@ def check(path: Path, family: str, full: bool):
     name = font["name"]
     assert name.getDebugName(1) == family, name.getDebugName(1)
     assert name.getDebugName(6) == family.replace(" ", "") + "-Regular"
-    assert name.getDebugName(5) == "Version 0.403" and abs(font["head"].fontRevision - 0.403) < 1e-4
+    assert name.getDebugName(5) == "Version 0.500" and abs(font["head"].fontRevision - 0.5) < 1e-4
     assert "Klee Project Authors" in name.getDebugName(0) and name.getDebugName(13).startswith("This Font Software")
     assert all(c in cmap for c in [*range(0x30A1, 0x30FB), *range(0x31F0, 0x3200), 0x3099, 0x309A, 0x309B, 0x309C, 0x30F0, 0x30F1, 0x30F2, 0x30F4, 0x1B127, 0x1B128, 0x2A708, 0x3031, 0x3032])
     if not full:
